3062f0d20bce6b42c68dee4a542f5861a65dff0b
ashkankzme <ashkankzme@gmail.com>
2024-08-14 13:24:57 -0700

    WIP: training code started

--- src/train.py
+++ src/train.py
@@ -1,5 +1,11 @@
-from zeta_zeros import zeta_zeros
+import json
+
+
+def load_json_file(filepath: str) -> list[list[int]]:
+    with open(filepath, 'r') as file:
+        return json.load(file)
 
 
 if __name__ == '__main__':
-    zeros = zeta_zeros()
+    zeta_zero_trajectories = load_json_file('../data/zz_trajectories_2m.json')
+    print(f"Number of trajectories: {len(zeta_zero_trajectories)}")
